3aad43bd8923e9cad03f5775a2d83930f351d5c9
HEISENBERG <satya@taksykraft.com>
2019-06-28 14:16:28 +0530

    patch

---
+++ handler/main.go
@@ -0,0 +1,16 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"play/utility"
+)
+
+// IndexHandler data
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Server", "GO BAT 🦇🦇🦇🦇🦇🦇🦇")
+	w.Header().Set("Content-Type", "application/json")
+	data, _ := json.Marshal(utility.ReadJson("database/data.json"))
+	fmt.Fprint(w, string(data))
+}
--- main.go
+++ main.go
@@ -1,21 +1,11 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
-	"play/utility"
+	"play/handler"
 )
 
-// IndexHandler data
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Server", "GO BAT 🦇🦇🦇🦇🦇🦇🦇")
-	w.Header().Set("Content-Type", "application/json")
-	data, _ := json.Marshal(utility.ReadJson("database/data.json"))
-	fmt.Fprint(w, string(data))
-}
-
 func main() {
-	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/", handler.IndexHandler)
 	http.ListenAndServe(":80", nil)
 }
